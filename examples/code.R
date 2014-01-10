23c3c2631f5807400f148b3a1869c6c2de58f738
timelyportfolio <kent.russell@timelyportfolio.com>
2014-01-10 16:20:32 -0600

    defaultColors in dimple new from other branch; need to figure out

--- examples/code.R
+++ examples/code.R
@@ -1,6 +1,8 @@
 #if you have not installed the newest dev branch of rCharts
 #require(devtools)
-#install_github("rCharts","ramnathv",ref="dev")
+#install_github("rCharts","timelyportfolio",ref="objeq")
+#setwd("C:/Users/Kent.TLEAVELL_NT/Dropbox/development/r/rCharts")
+#install()
 
 library(rCharts)
 
@@ -12,7 +14,8 @@ d1 <- dPlot(
   data = hairEye,
   type = "bar"
 )
+#d1$set(defaultColors=c())
 d1$setLib(".")
-d1$templates$page = paste0(getwd(),"/rCharts_objeq.html")
+d1$templates$page = "rCharts_objeq.html"
 d1$templates$script = paste0(getwd(),"/chart_objeq.html")
 d1
