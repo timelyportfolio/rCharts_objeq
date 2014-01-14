18f6933320219309123552c22e9ebe6d33033746
timelyportfolio <kent.russell@timelyportfolio.com>
2014-01-14 13:15:47 -0600

    add scatter plot example using dimple sample dataset

--- examples/code.R
+++ examples/code.R
@@ -21,5 +21,38 @@ d1$templates$page = "rCharts_objeq.html"
 d1$templates$script = paste0(getwd(),"/chart_objeq.html")
 d1
 
-
 d1$save("rCharts_objeq_example.html",cdn=F)
+
+
+#get data used by dimple for all of its examples as a first test
+data <- read.delim(
+  "http://pmsi-alignalytics.github.io/dimple/data/example_data.tsv"
+)
+
+### eliminate . to avoid confusion in javascript
+colnames(data) <- gsub("[.]","",colnames(data))
+
+### example 16 Scatter
+d2 <- dPlot(
+  OperatingProfit~UnitSales,
+  groups = c("SKU","Channel"),
+  data = subset(data, Date == "01/12/2012"),
+  type = "bubble"
+)
+d2$xAxis( type = "addMeasureAxis" )
+d2$yAxis( type = "addMeasureAxis" )
+d2$legend(
+  x = 200,
+  y = 10,
+  width = 500,
+  height = 20,
+  horizontalAlign = "right"
+)
+d2
+d2$setLib(".")
+d2$templates$page = "rCharts_objeq.html"
+d2$templates$script = paste0(getwd(),"/chart_objeq.html")
+d2
+
+
+d2$save("rCharts_objeq_example2.html",cdn=F)
